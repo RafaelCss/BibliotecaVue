30b0ce7d28f3f99d45e9b86a8843cb7083307702
devspacerj <rf2014souza@gmail.com>
2023-02-27 23:35:13 -0300

    feature/27-02 iniciando o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Vamos começar o Teste" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
